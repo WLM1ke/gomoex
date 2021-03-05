1db9bacded45089b00a2281a28b5bdaff045df94
WLM1ke <wlmike@gmail.com>
2021-03-05 09:09:47 +0300

    feat: добавлен draft запроса истории котировок - использует курсор и многостраничную загрузку

--- history.go
+++ history.go
@@ -3,8 +3,6 @@ package gomoex
 import (
 	"context"
 	"encoding/json"
-	"strings"
-	"time"
 )
 
 // Ключевые плейсхолдеры запросов - полный справочник https://iss.moex.com/iss/index.json
@@ -13,8 +11,7 @@ const (
 	EngineCurrency = "currency" // Валютный рынок
 	EngineFutures  = "futures"  // Срочный рынок
 
-	MarketIndex = "index" // Индексы фондового рынка
-
+	MarketIndex         = "index"         // Индексы фондового рынка
 	MarketShares        = "shares"        // Рынок акций
 	MarketBonds         = "bonds"         // Рынок облигаций
 	MarketForeignShares = "foreignshares" // Иностранные ц.б.
@@ -29,40 +26,64 @@ const (
 	BoardTQTF = "TQTF" // Т+: ETF - безадрес.
 )
 
-const dates = "dates"
+// Ключевые таблицы
+const (
+	dates   = "dates"
+	history = "history"
+)
 
-type ISSDate struct {
-	time.Time
+type Dates struct {
+	From string `json:"from"`
+	Till string `json:"till"`
 }
 
-func (j *ISSDate) UnmarshalJSON(b []byte) (err error) {
-	s := strings.Trim(string(b), "\"")
-	j.Time, err = time.Parse("2006-01-02", s)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-type BoardDates struct {
-	From ISSDate `json:"from"`
-	Till ISSDate `json:"till"`
-}
-
-func (iss ISSClient) GetBoardDates(ctx context.Context, engine string, market string, board string) (table []BoardDates, err error) {
-	url := ISSQuery{
-		history:  true,
-		engine:   engine,
-		market:   market,
-		board:    board,
-		endPoint: dates,
-		table:    dates,
+// MarketDates получает таблицу с биапазоном торговых дат для данного рынка.
+// Описание запроса - https://iss.moex.com/iss/reference/83
+func (iss ISSClient) MarketDates(ctx context.Context, engine string, market string) (table []Dates, err error) {
+	query := ISSQuery{
+		history: true,
+		engine:  engine,
+		market:  market,
+		object:  dates,
+		table:   dates,
 	}
 
-	rows, errors := iss.getAll(ctx, url)
+	rows, errors := iss.getAll(ctx, query)
 
 	for rawRow := range rows {
-		table = append(table, BoardDates{})
+		table = append(table, Dates{})
+		err = json.Unmarshal(rawRow, &table[len(table)-1])
+		if err != nil {
+			return nil, err
+		}
+	}
+	if err = <-errors; err != nil {
+		return nil, err
+	}
+	return table, nil
+}
+
+type Quotes struct {
+	Date  string  `json:"TRADEDATE"`
+	Close float64 `json:"CLOSE"`
+}
+
+// MarketHistory исторические котировки для данного инструмента и всех торговоых режимов для данного рынка.
+// Описание запроса - https://iss.moex.com/iss/reference/63
+func (iss ISSClient) MarketHistory(ctx context.Context, engine string, market string, security string) (table []Quotes, err error) {
+	query := ISSQuery{
+		history:   true,
+		engine:    engine,
+		market:    market,
+		security:  security,
+		table:     history,
+		multipart: true,
+	}
+
+	rows, errors := iss.getAll(ctx, query)
+
+	for rawRow := range rows {
+		table = append(table, Quotes{})
 		err = json.Unmarshal(rawRow, &table[len(table)-1])
 		if err != nil {
 			return nil, err
